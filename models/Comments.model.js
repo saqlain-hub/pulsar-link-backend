cf3f8824183628110a3316c469493a902e3d29d3
Saqlain Javed <saqlainsj12@gmail.com>
2023-12-20 18:47:51 +0500

    models

--- models/Comments.model.js
+++ models/Comments.model.js
@@ -1,6 +1,12 @@
-const commentSchema = new Schema({
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
   content: String,
   timestamp: { type: Date, default: Date.now },
   author: { type: Schema.Types.ObjectId, ref: "User" },
   post: { type: Schema.Types.ObjectId, ref: "Post" },
 });
+
+const Comments = mongoose.model("Comments", commentSchema);
+
+module.exports = Comments;
